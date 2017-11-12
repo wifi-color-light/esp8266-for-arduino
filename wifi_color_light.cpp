08b43340052b7400f7533a6c8e44c1e7ecb4c21e
guihui <494723324@qq.com>
2017-11-12 22:49:18 +0800

    修改主方法源码文件，初步构建具体业务逻辑代码框架

--- wifi_color_light.cpp
+++ wifi_color_light.cpp
@@ -1,6 +1,11 @@
 #include <ESP8266WiFi.h>
+#include <ESP.h>
+#include <stdbool.h>
+#include "button.h"
 #include "wifi_color_light.h"
 #include "AP_Service.h"
+#include "STA_Service.h"
+
 const char* ssid = "360WiFi-D9B905";
 const char* password = "guihui1104.";
 //The setup function is called once at startup of the sketch
@@ -14,8 +19,9 @@ void setup()
 	Serial.print("connecting to:");
 
 	Serial.println(ssid);
-	/*WiFi.begin(ssid,password);
-
+	WiFi.mode(WIFI_STA);
+	WiFi.begin(ssid,password);
+	setButtonEvent(Button::clear);
 	while(WiFi.status() != WL_CONNECTED)
 	{
 		delay(500);
@@ -23,20 +29,46 @@ void setup()
 	}
 	Serial.println("\nW iFi connected");
 
-	server.begin();
+	/*server.begin();
 	Serial.println("Server started");*/
 
 
 	//server.begin();
 
 }
-
+namespace User
+{
+	enum Mode
+	{
+		config,
+		normal,
+	};
+}
 // The loop function is called in an endless loop
 void loop()
 {
+	static User::Mode mode = User::normal;
+
+	if (checkButtonEvent() == Button::press)
+	{
+		mode = User::config;
+		clearButtonEvent();
+	}
 
 //Add your repeated code here
-	AP_Service("guihui","guihui00",80);
+	if (mode == User::config)
+	{
+		if(AP_Service("guihui","guihui00",80) == 0)
+		{
+			EspClass esp;
+			esp.reset();
+		}
+	}
+	else if (mode == User::normal)
+	{
+		STA_Service();
+		Serial.println("STA_Service");
+	}
 
 	delay(500);
 }
