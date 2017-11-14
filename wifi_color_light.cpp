3610fed215e1714a6677c49702ce6d8c30974de1
guihui <494723324@qq.com>
2017-11-15 00:21:14 +0800

    主文件

--- wifi_color_light.cpp
+++ wifi_color_light.cpp
@@ -3,11 +3,16 @@
 #include <stdbool.h>
 #include "button.h"
 #include "wifi_color_light.h"
+#include "osapi.h"
+#include "user_interface.h"
+#include "ets_sys.h"
+#include "mem.h"
 #include "AP_Service.h"
 #include "STA_Service.h"
-
+#include "time.h"
 const char* ssid = "360WiFi-D9B905";
 const char* password = "guihui1104.";
+
 //The setup function is called once at startup of the sketch
 WiFiServer server(80);
 void setup()
@@ -15,6 +20,8 @@ void setup()
 // Add your initialization code here
 	Serial.begin(115200);
 	delay(10);
+	int timezone = 3;
+	int dst = 0;
 
 	Serial.print("connecting to:");
 
@@ -28,7 +35,7 @@ void setup()
 		Serial.print(".");
 	}
 	Serial.println("\nW iFi connected");
-
+	buttonInitialization();
 	/*server.begin();
 	Serial.println("Server started");*/
 
@@ -66,9 +73,12 @@ void loop()
 	}
 	else if (mode == User::normal)
 	{
-		STA_Service();
-		Serial.println("STA_Service");
+		//STA_Service();
+		//Serial.println("STA_Service");
 	}
 
-	delay(500);
+	 time_t now = time(nullptr);
+	 Serial.println(ctime(&now));
+	 Serial.println(micros()/1000000);
+	delay(3000);
 }
